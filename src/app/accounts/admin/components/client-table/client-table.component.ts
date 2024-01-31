27b938934bab092ea9e1a217cfb5a82034de0fb9
Hubert-Travis <hubert.agbenyegah@amalitech.com>
2024-01-31 12:59:01 +0000

    project and client table changes

--- src/app/accounts/admin/components/client-table/client-table.component.ts
+++ src/app/accounts/admin/components/client-table/client-table.component.ts
@@ -14,6 +14,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './client-table.component.css',
 })
 export class ClientTableComponent implements OnInit {
+
   totalPages: number = 0;
   currentPage: number = 1;
   itemsPerPage: number = 10;
